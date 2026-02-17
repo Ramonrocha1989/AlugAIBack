afdd0891d7b37e2e8a54c177d27c2282554d0c66
Ramon Rocha <ramon.rocha@compasso.com.br>
2026-02-17 16:15:13 -0300

    test: altera preço do plano lojista para R$ 4

--- src/modules/plans/plans.service.ts
+++ src/modules/plans/plans.service.ts
@@ -25,7 +25,7 @@ export class PlansService {
     {
       id: 'lojista',
       name: 'Lojista',
-      price: 4,
+      price: 1,
       maxAds: -1,
       features: [
         'Anúncios ilimitados',
